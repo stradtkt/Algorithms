e54c2764f9a601847286f5862358a146cbc0c383
Kevin <stradtkt22@gmail.com>
2018-05-19 15:24:45 -0400

    Added reverseArray.js

--- AlgorithmApp/chapter-1-fundementals/reverseArray.js
+++ AlgorithmApp/chapter-1-fundementals/reverseArray.js
@@ -1,4 +1,11 @@
+// function reverseArray(arr) {
+//  return arr.reverse();
+// }
 function reverseArray(arr) {
- return arr.reverse();
+  var newArr = [];
+  for(var i = arr.length-1; i >=0; i--) {
+    newArr.push(arr[i]);
+  }
+  return newArr;
 }
 console.log(reverseArray([1,2,3,4,5,6]));
