66286f8df40340cfa38f2c7072a59cc7242c9629
Kevin <stradtkt22@gmail.com>
2018-05-20 06:41:49 -0400

    Added maxminaverage.js

---
+++ AlgorithmApp/TheBasic13/maxMinAvg.js
@@ -0,0 +1,14 @@
+function maxMinAvg(arr) {
+  var max = arr[0], min = arr[0], sum = arr[0];
+  for(var i = 1; i < arr.length; i++) {
+    if(arr[i] > max) {
+      max = arr[i];
+    }
+    if(arr[i] < min) {
+      min = arr[i];
+    }
+    sum += arr[i];
+  }
+  return [max, min, sum/arr.length];
+}
+console.log(maxMinAvg([1,2,3,4,5,6]));
