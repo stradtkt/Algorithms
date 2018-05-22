77eaa5b8c985cba78447b460035a2a34a4ad5aa2
Kevin <stradtkt22@gmail.com>
2018-05-22 14:44:04 -0400

    twelveBarBlues.js added

---
+++ AlgorithmApp/chapter-2/twelveBarBlues.js
@@ -0,0 +1,9 @@
+function twelveBarBlues() {
+  for(var i = 0; i <= 12; i++) {
+    console.log(i);
+    console.log("chick");
+    console.log("boom");
+    console.log("chick");
+  }
+}
+twelveBarBlues();
