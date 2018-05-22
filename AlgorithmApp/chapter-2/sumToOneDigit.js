3d568ae57f46d057c1a3e358f16808c7d6babf43
Kevin <stradtkt22@gmail.com>
2018-05-22 15:42:21 -0400

    sumToOneDigit.js added

---
+++ AlgorithmApp/chapter-2/sumToOneDigit.js
@@ -0,0 +1,12 @@
+function sumToOneDigit(num) {
+  if(num < 10) {
+    return num;
+  }
+  var str = num.toString();
+  var sum = 0;
+  for(var i = 0; i < str.length; i++) {
+    sum += Number(str[i]);
+  }
+  return sumToOneDigit(sum);
+}
+console.log(sumToOneDigit(1239));
