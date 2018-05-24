f306e9a445dcfcaa2286dc852b39a82ee2613d4a
Kevin <stradtkt22@gmail.com>
2018-05-23 21:43:54 -0400

    Added removeAt.js

---
+++ AlgorithmApp/chapter-3/removeAt.js
@@ -0,0 +1,10 @@
+function removeAt(arr, idx) {
+  var temp = arr[idx];
+  for(var i = idx; i < arr.length; i++) {
+    arr[i] = arr[i+1];
+  }
+  arr.length--;
+  console.log(arr);
+  return temp;
+}
+console.log(removeAt([1,2,3,4,5,6],3));
