67428feeabde0ec27888818fa6d866ee860b62c9
Kevin <stradtkt22@gmail.com>
2018-05-26 10:02:35 -0400

    Added removeNegatives2.js

--- AlgorithmApp/chapter-3/removeNegatives.js
+++ AlgorithmApp/chapter-3/removeNegatives.js
@@ -11,3 +11,17 @@ function removeNegatives(arr) {
   return arr;
 }
 console.log(removeNegatives([-1,-2,-3,4,5,6,7]));
+//output [4,5,6,7]
+
+function removeNegatives2(arr) {
+  var newArr = [];
+  for(var i = 0; i < arr.length; i++) {
+    if(arr[i] >= 0) {
+      newArr.push(arr[i]);
+    }
+  }
+  arr = newArr;
+  return newArr;
+}
+console.log(removeNegatives2([-1,-2,-3,4,5,6,7,8]));
+//output [4,5,6,7,8]
