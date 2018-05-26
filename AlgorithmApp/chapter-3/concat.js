a92fb5e1ce39d32f3763a1840eaee656d96c7d60
Kevin <stradtkt22@gmail.com>
2018-05-26 09:06:18 -0400

    Added concat.js

---
+++ AlgorithmApp/chapter-3/concat.js
@@ -0,0 +1,7 @@
+function concat(arr1, arr2) {
+  for(var i = 0; i < arr2.length; i++) {
+    arr1.push(arr2[i]);
+  }
+  return arr1;
+}
+console.log(concat([1,2,3,4,5], ["Hello", "my", "name", "is", "kevin"]));
