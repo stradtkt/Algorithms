b49d220f11facadb8477780c4bbc74912189f1e5
Kevin <stradtkt22@gmail.com>
2018-05-29 13:48:54 -0400

    Added creditCardValidation.js

---
+++ AlgorithmApp/chapter-3/creditCardValidation.js
@@ -0,0 +1,20 @@
+function isCreditCardValid(digitArr) {
+  var lastDigit = digitArr.pop();
+  var sum = 0;
+  for(var i = digitArr.length-1; i > 0; i-=2) {
+    digitArr[i] = digitArr[i] * 2;
+  }
+  for(var i = 0; i < digitArr.length; i++) {
+    if(digitArr[i] > 9) {
+      digitArr[i] -= 9;
+    }
+    sum += digitArr[i];
+  }
+  sum += lastDigit;
+  if(sum % 10 == 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+console.log(isCreditCardValid([5,2,2,8,2]));
