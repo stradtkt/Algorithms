07ac364ad94a74c29fbe5bf3f6e8b41302a51f53
Kevin <stradtkt22@gmail.com>
2018-06-02 00:00:52 -0400

    Added acronyms function

--- AlgorithmApp/chapter-4/answers.js
+++ AlgorithmApp/chapter-4/answers.js
@@ -44,3 +44,27 @@ function getDigits(str) {
   return numStr; 
 }
 console.log(getDigits("0s1a3y5w7h9a2t4?6!8?0"));
+
+/**
+ * Acronyms
+ * Create a function that, given a string, returns the string’s acronym (first letters only, capitalized).
+ * Given " there's no free lunch - gotta pay yer way. " , return "TNFL-GPYW" . 
+ * Given "Live from New York, it's Saturday Night!" , return "LFNYISN" .
+ */
+
+ function acronyms(str) {
+   var str2 = "";
+   for(var i = 0; i < str.length-1; i++) {
+     if(str[i+1] !== " " && str[i] === " ") {
+       str2 += str[i+1].toUpperCase();
+     }
+   }
+   str = str2;
+   return str;
+ }
+ console.log(acronyms("there's no free lunch - gotta pay yer way. "));
+ console.log(acronyms(" Young Money Cash Money Ballers"));
+ console.log(acronyms(" Live from New York, it's Saturday Night!"));
+
+
+ 
