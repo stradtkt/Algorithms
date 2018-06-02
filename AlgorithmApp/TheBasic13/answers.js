73e80350fddc45a205ef807052f94e881c5ad053
Kevin <stradtkt22@gmail.com>
2018-06-02 15:33:57 -0400

    Added basic13

--- AlgorithmApp/TheBasic13/answers.js
+++ AlgorithmApp/TheBasic13/answers.js
@@ -28,3 +28,48 @@
    return sum;
  }
 //  console.log(printIntsAndSum0To255());
+
+/**
+ * Print Max of Array
+ * printMaxOfArray(arr)
+ * Given an array, find and print its largest element.
+ */
+
+ function maxOfArray(arr) {
+  return Math.max(...arr);
+ }
+//  console.log(maxOfArray([1,2,3,4,5]));
+ //output 5
+
+ /**
+  * Return Odds Array 1-255
+  * returnOddsArray1To255()
+  * Create an array with all the odd integers between 1 and 255 (inclusive).
+  */
+
+  function returnsOddsArray1To255() {
+    var start = 1, end = 255;
+    for(var i = start; i < end; i++) {
+      if(i % 2 !== 0) {
+        console.log(i);
+      }
+    }
+  }
+  // console.log(returnsOddsArray1To255());
+
+  /**
+   * Return Array Count Greater than Y
+   * returnArrayCountGreaterThanY(arr, y)
+   * Given an array and a value Y, count and print the number of array values greater than Y.
+   */
+
+   function valuesGreaterThanY(arr, Y) {
+     for(var i = 0; i < arr.length; i++) {
+       if(arr[i] > Y) {
+         console.log(arr[i]);
+       }
+     }
+     return arr;
+   }
+// valuesGreaterThanY([1,22,33,44,55,66,77,88],44);
+
