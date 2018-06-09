8611e7fa55571a8a00e8bc76af66aa915b45708f
Kevin <stradtkt22@gmail.com>
2018-06-09 00:28:12 -0400

    Added more to chapter-1 answers.js

--- AlgorithmApp/chapter-1-fundementals/answers.js
+++ AlgorithmApp/chapter-1-fundementals/answers.js
@@ -13,4 +13,30 @@
     console.log("My name is now " + myName);
     console.log("My number is now " + myNumber);
  }
- settingAndSwapping();
+//  settingAndSwapping();
+
+/**
+ * Print -52 to 1066
+ * Print integers from -52 to 1066 using a FOR loop.
+ */
+
+ function printNegative52to1066() {
+   var start = -52, end = 1066;
+   for(var i = start; i <= end; i++) {
+     console.log(i);
+   }
+ }
+//  printNegative52to1066();
+
+/**
+ * Don’t Worry, Be Happy
+ * Create beCheerful() . Within it, console.log string "good morning!" Call it 98 times.
+ */
+
+ function beCheerful() {
+    for(var i = 0; i <= 98; i++) {
+      console.log("good morning!");
+    }
+ }
+//  beCheerful();
+
