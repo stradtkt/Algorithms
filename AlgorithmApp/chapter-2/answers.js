48cc5b4d255d00c4377a2541fd38389417990ece
Kevin <stradtkt22@gmail.com>
2018-06-09 08:52:43 -0400

    Added more to chapter-2 answer.js

--- AlgorithmApp/chapter-2/answers.js
+++ AlgorithmApp/chapter-2/answers.js
@@ -33,3 +33,57 @@ function factorial(num) {
 //output 120
 // console.log(factorial(6));
 //output 720
+
+/**
+ * Star Art
+ * Assume that you have a text field that is exactly 75 characters long. 
+ * You want to fill it with spaces and asterisks ( '*' ), sometimes called stars . 
+ * You should print the given number of asterisks consecutively. Depending on which 
+ * function is called, those stars should be left-justified (first star would be very first char in the text field), 
+ * or right-justified (last star would be very last char in the text field, with potentially some number 
+ * of spaces at beginning of text field before the block of stars start), or centered in the 75-character 
+ * text field (with same number of spaces on either side of the block of stars, plus/minus one).
+ * Write a function drawLeftStars(num) that accepts a number and prints that many asterisks.
+ * Write a function drawRightStars(num) that prints 75 characters total. Stars should build from right side.
+ *  The last num characters should be asterisks; the other 75 should be spaces.
+ * Write function drawCenteredStars(num) that prints 75 characters total. 
+ * The stars should be centered in the 75. The middle num characters should be asterisks; the rest
+ * of the 75 spaces.
+ * (optional) Create epic text-art Empire vs. Rebellion battle scenes, with ships like
+ * ( =*=) and >o< .
+ */
+function drawLeftStars(num) {
+  var stars = '';
+  for(var i = 1; i <= num; i++) {
+    stars += "*";
+  }
+  console.log(stars);
+}
+// drawLeftStars(60);
+
+function drawRightStars(num) {
+  var stars = '';
+  for(var i = 1; i <= 75-num; i++) {
+    stars += " ";
+  }
+  for(var i = 1; i <= num; i++) {
+    stars += "*";
+  }
+  console.log(stars);
+}
+// drawRightStars(10);
+
+function drawCenteredStars(num) {
+  var stars = '';
+  for(var i = 1; i <= Math.ceil(75/2-num/2); i++) {
+    stars += " ";
+  }
+  for(var i = 1; i <= num; i++) {
+    stars += "*";
+  }
+  for(var i = 1; i <= Math.ceil(75/2-num/2); i++) {
+    stars += " ";
+  }
+  console.log(stars);
+}
+// drawCenteredStars(30);
