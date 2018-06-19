f8d89f343b1068d0d93b5875a98c92422ebb25cc
Kevin <stradtkt22@gmail.com>
2018-06-19 12:28:38 -0400

    rocking the dojo sweatshirt to chapter-2 answers.js

--- AlgorithmApp/chapter-2/answers.js
+++ AlgorithmApp/chapter-2/answers.js
@@ -157,3 +157,31 @@ function drawCenteredStars(num) {
     return true;
   }
   console.log(isPrime(11));
+
+  /**
+   * Rockin’ the Dojo Sweatshirt
+   * Ever since you arrived at the Dojo, you wanted one of 
+   * those cool Coding Dojo sweatshirts – maybe even more 
+   * than one. Let’s say they cost $20 (including tax), but friendly 
+   * Josh gives a 9% discount if you buy two, a nice 19% discount
+   * if you buy three, or a sweet 35% discount if you buy four or more. 
+   * He only accepts cash and says he doesn’t have coins, so you should 
+   *    round up to the nearest dollar. Build function sweatshirtPricing(num) that, 
+   * given a number of sweatshirts, returns the cost.
+   */
+
+   function rockinDojoSweatshirt(num) {
+     var cost = 20;
+     var discount = [
+       [2, 0.09],
+       [3, 0.19],
+       [4, 0.35]
+     ];
+     for(var i = 0; i < discount.length; i++) {
+       if(num == discount[i][0]) {
+         return num * cost * (1 - discount[i][1]);
+       }
+     }
+     return num * cost;
+   }
+   console.log(rockinDojoSweatshirt(3));
