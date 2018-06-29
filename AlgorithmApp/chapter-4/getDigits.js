238eacbbaa56dbd3dbc8ead0321e761986bbb6ee
Kevin <stradtkt22@gmail.com>
2018-06-29 14:28:14 -0400

    Added getDigits to chapter-4

--- AlgorithmApp/chapter-4/getDigits.js
+++ AlgorithmApp/chapter-4/getDigits.js
@@ -5,5 +5,23 @@
  */
 
  function getDigits(str) {
-   
+   var numStr = "";
+   for(var i = 0; i < str.length; i++) {
+     switch(str[i]) {
+       case "0":
+       case "1":
+       case '2':
+       case '3':
+       case '4':
+       case '5':
+       case '6':
+       case '7':
+       case '8':
+       case '9':
+        numStr += str[i];
+     }
+   }
+   numStr = Number(numStr);
+   return numStr;
  }
+ console.log(getDigits('dfkjn5334j3kj34kj34k3h4k3hk43h'));
