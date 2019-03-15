49bedd89ad289a84e0aab91675bfc685d1e8d7cc
Kevin <stradtkt22@gmail.com>
2019-03-15 15:10:38 -0400

    Added removeNegatives to the linked lists

---
+++ AlgorithmApp/chapter-5/removeNegatives.js
@@ -0,0 +1,64 @@
+/**
+ * SList: Remove Negatives
+ * Given a pointer to the head node of a singly linked list, 
+ * remove any nodes containing negative values and return (a pointer to) the new list.
+ */
+
+function Node(value){
+    this.val = value;
+    this.next = null;
+}
+
+function SLL(){
+    this.head = null;
+
+    this.add = function(value) {
+        var newNode = new Node(value);
+        if(!this.head) {this.head = newNode;}
+        else {
+            var runner = this.head;
+            while (runner.next){
+                runner = runner.next;
+            }
+            runner.next = newNode;
+        }
+        return this;
+    }
+
+    this.removeNegatives = function(){
+
+        while (this.head.val < 0){
+            this.head = this.head.next;
+        }
+
+        var runner = this.head;
+
+        while (runner.next){
+            if (runner.next.val < 0){
+                runner.next = runner.next.next;
+            }
+            else {
+                runner = runner.next;
+            }
+        }
+
+        return this;
+    }
+
+    this.printAsArray = function(){
+        var runner = this.head;
+        var arr = [];
+        while (runner){
+            arr.push(runner.val);
+            runner = runner.next;
+        }
+        console.log(arr);
+        return this;
+    }
+}
+
+var sList = new SLL();
+sList.add(-1).add(-2).add(3).add(-4).add(-5).add(-6).add(7);
+sList.printAsArray();
+sList.removeNegatives();
+sList.printAsArray();
